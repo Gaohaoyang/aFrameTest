7137df93f3c4f5de0386cf9f07d67c6295af7913
chuan shi(haoyang gao) <ghy118432@alibaba-inc.com>
2017-01-03 00:01:06 +0800

    test video

---
+++ index.js
@@ -0,0 +1,8 @@
+var boxEl = document.querySelector('a-box')
+boxEl.addEventListener('mouseenter', function() {
+    boxEl.setAttribute('scale', {
+        x: 2,
+        y: 2,
+        z: 2
+    });
+});
